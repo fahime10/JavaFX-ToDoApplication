bf71edae3b79030cb30bb0958f7f80abceb7ea8b
Fahim Ahmed <fahim7711@gmail.com>
2023-12-14 18:30:18 +0000

    Added initial files

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.todoapplication {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+            requires validatorfx;
+                requires org.kordamp.bootstrapfx.core;
+            
+    opens com.example.todoapplication to javafx.fxml;
+    exports com.example.todoapplication;
+}
